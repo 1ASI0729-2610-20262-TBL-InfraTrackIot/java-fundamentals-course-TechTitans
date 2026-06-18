230afd5e135388d078cf2a01a66637f70bcc6f4a
Jefferson Morales <jefferson2k04@gmail.com>
2026-06-18 04:51:30 -0500

    feat: Create Methods.java

---
+++ Introduction-to-Programming-and-Java-Course/lesson5-methods/starter-files/Methods.java
@@ -0,0 +1,17 @@
+public class Methods {
+    
+    // TODO: 1. Crea aquí el método 'saludarUsuario'
+    // Debe ser 'public static void' y recibir un String 'nombre' y un int 'edad'.
+    // Dentro, usa System.out.println para imprimir el saludo.
+    
+
+    public static void main(String[] args) {
+        System.out.println("--- Inicio del Programa ---");
+        
+        // TODO: 2. Llama al método 'saludarUsuario' 3 veces con diferentes nombres y edades.
+        // Ejemplo: saludarUsuario("Carlos", 15);
+        
+
+        System.out.println("--- Fin del Programa ---");
+    }
+}
